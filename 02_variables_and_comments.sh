9f1d23806c79c0adb2f398dacbf51baf68cefbc5
Asnim <asnim96@gmail.com>
2019-06-21 22:58:14 +0530

    Added Variables & Comments Example

---
+++ 02_variables_and_comments.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# This is a comment
+
+# Variables Example
+echo "HelloWorld"
+
+echo $BASH
+echo $BASH_VERSION
+echo $HOME
+echo $PWD
+# The above are some of the system
+# variables in a linux machine
+
+
+# Custom Variables
+# The variable name shouldnt start with a number
+name=Mark
+
+val=210
+echo The name is $name
+echo The number is $val
